321f47715994f763795a1e1c3352d6bed94ee722
lbasora <luis.basora@onera.fr>
2020-08-29 14:43:23 +0200

    package creation+setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pytorch_convolutional_lstm",
+    description="Pytorch Convolutional LSTM",
+    packages=find_packages(),
+    install_requires=["torch"],
+)
